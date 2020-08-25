f7d7f2684ac2067dc4e7bc0fac45e0418d41e104
Sadia-web <sadiamim354@gmail.com>
2020-08-25 17:03:43 +0600

    finish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Country from './components/Country/Country';
+import Cart from './components/Cart/Cart';
 
 function App() {
+ const [countries, setCountries] = useState([]);
+ const [cart, setCart] = useState([]);
+
+ useEffect(() => {
+   fetch('https://restcountries.eu/rest/v2/all')
+   .then(res => res.json())
+   .then(data => setCountries(data))
+   .catch(error => console.log(error))
+ }, [])
+
+ const handleAddCountry = (country) => {
+   const newCart = [...cart, country];
+   setCart (newCart);
+ }
   return (
     <div className="App">
+      <h1>Country Loaded: {countries.length}</h1>
+      <h4>Country Added: {cart.length}</h4>
+      <Cart cart={cart}></Cart>
+
+      <ul>
+        {
+          countries.map(country => <Country country={country} handleAddCountry={handleAddCountry} key={country.alpha3code}></Country>)
+        }
+      </ul>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -21,6 +47,5 @@ function App() {
       </header>
     </div>
   );
-}
-
+      }
 export default App;
