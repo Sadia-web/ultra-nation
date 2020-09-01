f20fa5b2f2ed188880b2455a61406e721e9ad027
Sadia-web <sadiamim354@gmail.com>
2020-09-01 17:38:41 +0600

    remove logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Country from './components/Country/Country';
 import Cart from './components/Cart/Cart';
@@ -30,21 +29,6 @@ function App() {
           countries.map(country => <Country country={country} handleAddCountry={handleAddCountry} key={country.alpha3code}></Country>)
         }
       </ul>
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
       }
